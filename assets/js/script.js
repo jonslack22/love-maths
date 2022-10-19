3958a6899b2bc07119a552881b37d17f9a36fcd5
jonslack22 <jonathanslack89@ymail.com>
2022-10-19 21:09:50 +0000

    Add code for division questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,10 +44,13 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractionQuestion(num1, num2);
-    } else {    
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    } else {        
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 /**
@@ -89,7 +92,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"]; 
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {    
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"]
+    } else {        
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`; 
     }
@@ -138,4 +143,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    
+    document.getElementById("operand1").textContent = operand1 * operand2;
+	document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";
+
+    
 }
